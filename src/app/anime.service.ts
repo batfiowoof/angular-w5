ee1ef09f81433a4a9ff0c39922d811ad732978a2
batfiowoof <bojidarlambov@abv.bg>
2024-05-29 16:01:13 +0300

    Initial commmit / working fetching data

---
+++ src/app/anime.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnimeService {
+  private baseUrl = 'https://api.jikan.moe/v4/anime';
+
+  constructor(private http: HttpClient) {}
+
+  getAnime(id: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/${id}`);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { AnimeService } from './anime.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule],
+  styleUrls: ['./app.component.css'],
+  standalone: true,
 })
-export class AppComponent {
-  title = 'angular-w5';
+export class AppComponent implements OnInit {
+  anime: any;
+
+  constructor(private animeService: AnimeService) {}
+
+  ngOnInit(): void {
+    this.fetchAnime(1); // Replace 1 with the desired anime ID
+  }
+
+  fetchAnime(id: number): void {
+    this.animeService.getAnime(id).subscribe(
+      (data: any) => (this.anime = data.data),
+      (error) => console.error('Error fetching anime', error)
+    );
+  }
 }
